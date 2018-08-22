76db2b74a754a1b5fa93ce0bad2b915eaed48aec
Takuma6 <valock24dwerr@gmail.com>
2018-08-22 17:23:06 +0900

    update functions for hdf5

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 CXX = g++
 EIGEN_DIR = /usr/local/include/eigen3
+EIGEN_LINKS = -I $(EIGEN_DIR) 
+HDF_LINKS = -I/usr/local/include -L/usr/local/lib -lhdf5_hl -lhdf5
 
 all:
-	$(CXX) -I $(EIGEN_DIR) sample_poisson.cpp -o poisson.out
+	$(CXX) -std=c++11 poisson.cpp -o poisson.out $(EIGEN_LINKS) $(HDF_LINKS)
 
 clean:
 	rm -f *~ *.out
--- poisson.cpp
+++ poisson.cpp
@@ -7,7 +7,8 @@ What I need is to bring the function setting phi here (and maybe particle class)
 #include <iostream>
 #include <Eigen/IterativeLinearSolvers>
 #include <unsupported/Eigen/IterativeSolvers>
-#include "roll.h"
+//#include "roll.h"
+#include "SimIO.hpp"
 
 typedef Eigen::SparseMatrix<double> SpMat; // declares a column-major sparse matrix type of double
 typedef Eigen::Triplet<double> T;
@@ -21,10 +22,9 @@ double *eps, *phi, *phi_sum;
 
 class electric_coefficient
 {
-private:
+public:
     double eps_f, eps_t, eps_b;
     double sig_f, sig_t, sig_b;
-public:
     electric_coefficient();    //コンストラクタ
     void print_value();
 };
@@ -44,71 +44,14 @@ void input(int n1, int n2){
   Ns[1] = 1<<n2;
 }
 
-/*
-Functions for phi, epsilon
-*/
-
-void Make_phi_particle_sum(double *phi, double* phi_sum, Particle *p, const double radius){
-  int *nlattice;
-  nlattice = Ns;
-  Make_phi_particle_sum_primitive(phi, phi_sum, p, DX, NP_domain, Sekibun_cell, nlattice, radius);
-}
-
-inline void Make_phi_particle_sum_primitive(double *phi,
-                                            double *phi_sum,
-                                            Particle *p,
-                                            const double &dx,
-                                            const int &np_domain,
-                                            int **sekibun_cell,
-                                            const int Nlattice[DIM],
-                                            const double radius){
-#pragma omp parallel for 
-  for(int n = 0; n < Particle_Number; n++){
-    double xp[DIM];
-    for(int d = 0; d < DIM; d++) xp[d] = p[n].x[d];
-
-    int x_int[DIM];
-    double residue[DIM];
-    int sw_in_cell = Particle_cell(xp, dx, x_int, residue);
-    sw_in_cell = 1;
-    
-    int r_mesh[DIM];
-    double dmy, dmy_phi;
-    double r[DIM], x[DIM];
-    for(int mesh = 0; mesh < np_domain; mesh++){
-      Relative_coord(sekibun_cell[mesh], x_int, residue, sw_in_cell, Nlattice, dx, r_mesh, r);
-
-      for(int d = 0; d < DIM; d++) x[d] = r_mesh[d]*DX;
-
-      dmy     = Distance(x, xp);
-      dmy_phi = Phi(dmy, radius);
-
-#pragma omp atomic
-      phi_sum[(r_mesh[0] * NY * NZ_) + (r_mesh[1] * NZ_) + r_mesh[2]] += dmy_phi;
-    }
-  }
-
-  {
-#pragma omp parallel for
-    for(int i = 0; i < NX; i++){
-      int im;
-      for(int j = 0; j < NY; j++){
-        for(int k = 0; k < NZ; k++){
-          im = (i * NY * NZ_) + (j * NZ_) + k;
-          phi[im] = MIN(phi_sum[im], 1.0);
-        }
-      }
-    }
-  }
-}
-
 void Make_epsilon(electric_coefficient *ep, double *eps, double *phi_sum){
+  double eps_p = ep->eps_t;
   #pragma omp parallel for
-    for(int i = 0; i < NX; i++){
+    for(int i = 0; i < Nx; i++){
       int im;
-      for(int j = 0; j < NY; j++){
-        im = i * NY + j;
-        eps[im] = ep.eps_f + (ep.eps_p - ep.eps_f)*phi_sum[im]
+      for(int j = 0; j < Ny; j++){
+        im = i * Ny + j;
+        eps[im] = ep->eps_f + (eps_p - ep->eps_f)*phi_sum[im];
       }
     }
 }
@@ -116,30 +59,30 @@ void Make_epsilon(electric_coefficient *ep, double *eps, double *phi_sum){
 /*
 Functions for Poisson Eq.
 */
-inline void insertCoefficient(int id, int i, int j, double w, std::vector<T>& coeffs, Eigen::VectorXd& b, const int n){
+inline void insertCoefficient(int id, int i, int j, double w, std::vector<T>& coeffs, Eigen::VectorXd& b){
   int id1 = i*n+j;
-        if(i==-1 || i==n){
-        	i   = (i+n)%n;
-        	id1 =  i*n+j ;
+        if(i==-1 || i==Nx){
+        	i   = (i+Nx)%Nx;
+        	id1 =  i*Ny+j ;
         }
-  else  if(j==-1 || j==n){
-    			j   = (j+n)%j;
-    			id1 =  i*n+j ;
+  else  if(j==-1 || j==Ny){
+    			j   = (j+Ny)%j;
+    			id1 =  i*Ny+j ;
     		}
   coeffs.push_back(T(id,id1,w));              // unknown coefficient
 }
 
-void buildProblem(std::vector<T>& coefficients, Eigen::VectorXd& b, int n, int m){
+void buildProblem(std::vector<T>& coefficients, Eigen::VectorXd& b, int m){
   //b.setZero();
   b = Eigen::VectorXd::Random(m);
-  for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-      int id = i*n+j;
-      insertCoefficient(id, i-1,j,   -1, coefficients, b, n);
-      insertCoefficient(id, i+1,j,   -1, coefficients, b, n);
-      insertCoefficient(id, i  ,j-1, -1, coefficients, b, n);
-      insertCoefficient(id, i  ,j+1, -1, coefficients, b, n);
-      insertCoefficient(id, i  ,j  ,  4, coefficients, b, n);
+  for(int i=0; i<Nx; i++){
+    for(int j=0; j<Ny; j++){
+      int id = i*Ny+j;
+      insertCoefficient(id, i-1,j,   -1, coefficients, b);
+      insertCoefficient(id, i+1,j,   -1, coefficients, b);
+      insertCoefficient(id, i  ,j-1, -1, coefficients, b);
+      insertCoefficient(id, i  ,j+1, -1, coefficients, b);
+      insertCoefficient(id, i  ,j  ,  4, coefficients, b);
     }
   }
 }
@@ -155,11 +98,11 @@ void saveAsBitmap(const Eigen::VectorXd& x, int n, const char* filename){
 */
 
 int main(){
-  input(5, 5);
-	n = 1 << 5;  // size of the grid
-  m = n*n   ;  // number of unknows (=number of pixels)
-  electric_coefficient *ep = new electric_coefficient;
-  ep->print_value();
+  input(4, 5);
+  n = 1<<5   ;
+  m = Nx*Ny  ;  // number of unknows (=number of pixels)
+  //electric_coefficient *ep = new electric_coefficient;
+  //ep->print_value();
   //(*ep).print_values();
   /*
   // set phi, epsilon
@@ -172,12 +115,12 @@ int main(){
 	// Assembly:
 	std::vector<T> coefficients;            // list of non-zeros coefficients
 	Eigen::VectorXd b(m);                   // the right hand side-vector resulting from the constraints
-	buildProblem(coefficients, b, n, m);
+	buildProblem(coefficients, b, m);
 	SpMat A(m,m);
 	A.setFromTriplets(coefficients.begin(), coefficients.end());
 	
 
-	// fill A and b
+	// solve
 	Eigen::GMRES<SpMat> gmres(A);
 	Eigen::VectorXd x;
   gmres.setMaxIterations(3000);
@@ -187,7 +130,18 @@ int main(){
 	std::cout << "#iterations:     " << gmres.iterations() << std::endl;
 	std::cout << "estimated error: " << gmres.error()      << std::endl;
 	// update b, and solve again
-	// x = solver.solve(b);
+	// x = solver.solve(b)
 
+  // save to HDF5
+  double *dmy = new double [m];
+  for(int i=0; i<m; i++)dmy[i] = (double)x[i];
+
+  SimIO::SimIO fp;
+  fp.create_file("sample.h5");
+  fp.create_group("params");
+  fp.write_attr("dt", 1);
+  SimIO::io_space vectorSpace = SimIO::Space::create(Nx, Ny);
+  fp.write_data("velocity", vectorSpace, dmy);
+  fp.close_file();
 	return 0;
 }
